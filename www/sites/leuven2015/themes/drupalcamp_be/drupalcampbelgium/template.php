b73c19ae3695f37d9cac04bcacaa0a5773df2669
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2015-07-01 10:10:00 +0200

    DrupalCamp subtheme of Zurb Foundation 5.x

---
+++ www/sites/leuven2015/themes/drupalcamp_be/drupalcampbelgium/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function drupalcampbelgium_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function drupalcampbelgium_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function drupalcampbelgium_preprocess_node(&$variables) {
+}
