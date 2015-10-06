87f16a26b6238eb0957c62bb85d51d1f9f184c17
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2015-10-06 11:02:40 +0200

    Taxonomy page overview

--- sites/leuven2015/themes/drupalcamp_be/drupalcampbelgium/template.php
+++ sites/leuven2015/themes/drupalcamp_be/drupalcampbelgium/template.php
@@ -59,3 +59,35 @@ function drupalcampbelgium_preprocess_views_view(&$vars) {
     drupal_add_js('http://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js', 'external');
   }
 }
+
+/**
+ * Implements template_preprocess_field().
+ */
+function drupalcampbelgium_preprocess_field(&$variables, $hook) {
+  if ($variables['element']['#field_name'] == 'field_experience' && ($variables['element']['#view_mode'] == 'teaser' || $variables['element']['#view_mode'] == 'taxonomy_page_overview')) {
+    $variables['classes_array'][] = 'experience-level';
+    foreach ($variables['element']['#items'] as $item) {
+      $variables['classes_array'][] = 'experience-level-' . $item['value'];
+    }
+  }
+  elseif ($variables['element']['#field_name'] == 'og_vocabulary' && $variables['element']['#bundle'] == 'session' && $variables['element']['#view_mode'] == 'teaser') {
+    foreach ($variables['element']['#items'] as $key => $item) {
+      if (!empty($item['entity'])) {
+        $colors = field_get_items('taxonomy_term', $item['entity'], 'field_track_color');
+        if (!empty($colors) && !empty($colors[0]['rgb'])) {
+          $rgb = $colors[0]['rgb'];
+          $variables['item_attributes_array'][$key]['style'] = 'background: ' . $rgb;
+        }
+      }
+    }
+  }
+  elseif ($variables['element']['#field_name'] == 'title' && $variables['element']['#bundle'] == 'schedule_item') {
+    $node = $variables['element']['#object'];
+    $type = field_get_items('node', $node, 'field_schedule_item_type');
+
+    if (empty($type) || empty($type[0]['value'])) { return; }
+
+    $variables['classes_array'][] = 'schedule_item';
+    $variables['classes_array'][] = 'schedule_' . check_plain($type[0]['value']);
+  }
+}
